1fa07c707dee2de7550b4f459a61674478c8fa21
Marco J Git <marcojgit@gmx.de>
2019-02-06 14:47:31 +0100

    Testkommentar

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class Main {
     private static StockList stockList = new StockList();
+    
+    // Testkommentar
 
     public static void main(String[] args) {
 	    StockItem temp = new StockItem("bread", 0.86, 100);
